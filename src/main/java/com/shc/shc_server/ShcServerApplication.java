90dd0b16094b325ed57fa07aac48aa4ff33b4fe8
angc <angcoder.c@gmail.com>
2024-10-28 07:07:57 -0600

    migrate to postgressql

--- src/main/java/com/shc/shc_server/ShcServerApplication.java
+++ src/main/java/com/shc/shc_server/ShcServerApplication.java
@@ -1,12 +1,15 @@
 package com.shc.shc_server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ShcServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ShcServerApplication.class, args);
 	}
 
